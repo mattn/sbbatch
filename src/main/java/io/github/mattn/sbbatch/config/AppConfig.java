da17b4301cd6d815345e2e6fd778c3857f236402
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-09-07 02:46:53 +0900

    app prefix

--- src/main/java/io/github/mattn/sbbatch/config/AppConfig.java
+++ src/main/java/io/github/mattn/sbbatch/config/AppConfig.java
@@ -1,10 +1,14 @@
 package io.github.mattn.sbbatch.config;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by mattn on 2016/09/07.
  */
+@Component
 @Data
+@ConfigurationProperties(prefix = "app")
 public class AppConfig {
 }
